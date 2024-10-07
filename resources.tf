8b4d04e6b6d5cd2553d4848e0f9ba1e77bf0c1ef
Ian Knighton <iknighton@deseretdigital.com>
2024-10-07 08:20:23 -0600

    chore: add subs

--- resources.tf
+++ resources.tf
@@ -3,17 +3,17 @@ resource "google_pubsub_subscription" "subscription" {
   topic = var.topic_id
 
   dead_letter_policy {
-    dead_letter_topic     = google_pubsub_topic.dead_letter_topic.id
+    dead_letter_topic     = google_pubsub_topic.dead_letter_subscription_topic.id
     max_delivery_attempts = var.max_delivery_attempts
   }
 }
 
-resource "google_pubsub_topic" "dead_letter_topic" {
-  name   = "${var.topic_name}_DeadLetter"
+resource "google_pubsub_topic" "dead_letter_subscription_topic" {
+  name   = "${var.subscription_name}_DeadLetter"
   labels = var.labels
 }
 
 resource "google_pubsub_subscription" "dead_letter_subscription" {
   name  = "${var.subscription_name}_DeadLetter"
-  topic = google_pubsub_topic.dead_letter_topic.id
+  topic = google_pubsub_topic.dead_letter_subscription_topic.id
 }
