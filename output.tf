083e1a1a015bcf98d028798670133fcf0707a2ed
Ian Knighton <iknighton@deseretdigital.com>
2024-10-08 08:53:01 -0600

    chore: backdate version

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
 output "subscription_id" {
-  value = google_pubsub_subscription.subscription.id  
+  value = google_pubsub_subscription.subscription.id
 }
 
 output "subscription_name" {
-  value = google_pubsub_subscription.subscription.name  
+  value = google_pubsub_subscription.subscription.name
 }
